f780ff18bc0d91916777b56808dfe3411aaec886
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-26 23:46:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
 class App < Sinatra::Base
 
+def initialize
+end 
+
 	get '/' do
 		erb :index
 	end
 
+  get '/hello' do
+    erb :hello
+  end 
 
+  get '/goodbye' do
+    erb :goodbye
+    <%= <h1>Goodbye "#{name}"</h1>%>
+  end 
+  
+  get '/date' do 
+    erb :date
+    <%= timedate %>
+  end
+  
 end
